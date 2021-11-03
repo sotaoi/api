dcb2ec8adab033636b2f2b6ef328171edc8b61c6
qwertypnk <qwertypnk@protonmail.com>
2021-11-03 15:22:15 +0200

    refactor: update

--- db/cpanel-tables.ts
+++ db/cpanel-tables.ts
@@ -7,7 +7,7 @@ const cpanelMigrate = async (isMasterBundle: boolean): Promise<any> => {
     process.env.FLAG_MASTER_MIGRATION = 'yes';
     const migrations = (await Model.sdriver().migrate.latest())[1];
     !migrations.length && (migrations[0] = 'No new migrations');
-    logger().info(`\n\nMigrate command complete:\n${migrations.join('\n')}\n`);
+    logger().info(`\n\nMigrate command complete:\n${migrations.join('\n')}`);
   } catch (err) {
     logger().estack(err);
   }
@@ -20,7 +20,7 @@ const cpanelSeed = async (isMasterBundle: boolean): Promise<any> => {
   try {
     const seeds = (await Model.sdriver().seed.run())[0];
     !seeds.length && (seeds[0] = 'No seed files');
-    logger().info(`\n\nSeeding complete:\n${seeds.join('\n')}\n`);
+    logger().info(`\n\nSeeding complete:\n${seeds.join('\n')}`);
   } catch (err) {
     logger().estack(err);
   }
