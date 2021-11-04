c57e3de4218940036475b5ed9e5176a2c63415da
qwertypnk <qwertypnk@protonmail.com>
2021-11-04 14:00:28 +0200

    refactor: update

--- scripts/deploy/deploy-sapi.js
+++ scripts/deploy/deploy-sapi.js
@@ -6,30 +6,20 @@ const { Helper } = require('@sotaoi/omni/helper');
 const { execSync } = require('child_process');
 
 const main = async () => {
+  //
+
   fs.rmdirSync(path.resolve('./deployment'), { recursive: true });
-  fs.rmdirSync(path.resolve('./tmp.deployment'), { recursive: true });
   fs.mkdirSync(path.resolve('./deployment'));
   fs.writeFileSync(path.resolve('./deployment/.gitkeep'), '');
-
-  const packageJson = JSON.parse(fs.readFileSync(path.resolve('./package.json')).toString());
-
-  fs.mkdirSync(path.resolve('./tmp.deployment'));
-  execSync(`git clone git@github.com:sotaoi/api . && git checkout -b ${packageJson.version}`, {
-    cwd: path.resolve('./tmp.deployment'),
-    stdio: 'inherit',
-  });
 
   Helper.copyRecursiveSync(fs, path, path.resolve('./'), path.resolve('./deployment'), [
     path.resolve('.git'),
     path.resolve('./deployment'),
-    path.resolve('./certs'),
     path.resolve('./node_modules'),
-    path.resolve('./tmp.deployment'),
   ]);
 
   execSync('npx tsc', { cwd: path.resolve('./deployment'), stdio: 'inherit' });
   fs.unlinkSync(path.resolve('./deployment/tsconfig.json'));
-  execSync('npm run bootstrap:prod', { cwd: path.resolve('./deployment'), stdio: 'inherit' });
 
   Helper.iterateRecursiveSync(
     fs,
@@ -54,21 +44,7 @@ const main = async () => {
     [path.resolve('./deployment/node_modules')],
   );
 
-  fs.renameSync(path.resolve('./tmp.deployment/.git'), path.resolve('./deployment/.git'));
-  fs.rmdirSync(path.resolve('./tmp.deployment'), { recursive: true });
-  try {
-    execSync(
-      `git add --all && git commit -m "release ${packageJson.version}" && git push -f -u origin ${packageJson.version}`,
-      {
-        cwd: path.resolve('./deployment'),
-        stdio: 'inherit',
-      },
-    );
-  } catch (err) {
-    // do nothing
-    false && console.error(err);
-  }
-  fs.rmdirSync(path.resolve('./deployment/.git'), { recursive: true });
+  //
 };
 
 main();
